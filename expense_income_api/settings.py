91e89d467d8c7bd979ad8c295ff24a780e154988
Aditya Vikram Singh <adivik672@gmail.com>
2024-04-11 21:29:06 +0530

    Authentication App Added

--- expense_income_api/settings.py
+++ expense_income_api/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third-party app
+    "rest_framework",
+    # Local Apps
+    "authentication",
 ]
 
 MIDDLEWARE = [
