e0bd2ac0d05ee799381b1653508f50cc60950544
Aditya Vikram Singh <adivik672@gmail.com>
2024-04-11 17:00:54 +0530

    Postgresql Database Added

--- expense_income_api/settings.py
+++ expense_income_api/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "expense_income_api.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "expense_income_tracker",
+        "USER": "postgres",
+        "PASSWORD": "jaibhawani09",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
